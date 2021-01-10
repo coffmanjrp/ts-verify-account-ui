ce2536ccc611ac466665d93a08f925c65fb2d80d
coffmanjrp <coffmanjrp@gmail.com>
2021-01-10 10:53:41 -0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+var codes = document.querySelectorAll('.code');
+codes[0].focus();
+codes.forEach(function (code, idx) {
+    code.addEventListener('keydown', function (e) {
+        if (e.key >= 0 && e.key <= 9) {
+            codes[idx].value = '';
+            setTimeout(function () { return codes[idx + 1].focus(); }, 10);
+        }
+        else if (e.key === 'Backspace') {
+            setTimeout(function () { return codes[idx - 1].focus(); }, 10);
+        }
+    });
+});
