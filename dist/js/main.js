40a316222d90eac4ec83b8f0fb66d99447387c27
coffmanjrp <coffmanjrp@gmail.com>
2021-08-10 01:11:47 -0700

    Fix event key types

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ var codes = document.querySelectorAll('.code');
 codes[0].focus();
 codes.forEach(function (code, idx) {
     code.addEventListener('keydown', function (e) {
-        if (e.key >= 0 && e.key <= 9) {
+        if (+e.key >= 0 && +e.key <= 9) {
             codes[idx].value = '';
             setTimeout(function () { return codes[idx + 1].focus(); }, 10);
         }
